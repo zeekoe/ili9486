fc980bf6c7a3d9e8f066a39da614d4f385d36a46
zeekoe <git@ronaldteune.nl>
2022-02-22 13:43:47 +0100

    java test

--- wrapper.sh
+++ wrapper.sh
@@ -2,11 +2,10 @@
 
 rm -f *.o *.so
 
-# Compile armbian as shared library
+# Compile ili9486 as shared library
 gcc -c -Wall -O2 -fPIC ili9486.c
 
 # Link objects
 ld -shared ili9486.o -o ili9486.so
 
-# Compile JNA interface
-javac -cp jna-4.5.0.jar Ili9486.java
+sudo cp ili9486.so /usr/local/lib/
