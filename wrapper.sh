1289d3c463ed2bab30dad632b346d44eaa20e2aa
zeekoe <github@ronaldteune.nl>
2025-12-07 20:22:53 +0100

    correct build script

--- wrapper.sh
+++ wrapper.sh
@@ -2,10 +2,8 @@
 
 rm -f *.o *.so
 
-# Compile ili9486 as shared library
-gcc -c -Wall -O2 -fPIC ili9486.c
+gcc -O2 -fPIC -c ili9486.c 
+gcc -shared -o ili9486.so ili9486.o -lwiringPi
 
-# Link objects
-ld -shared ili9486.o -o ili9486.so
 
 sudo cp ili9486.so /usr/local/lib/
